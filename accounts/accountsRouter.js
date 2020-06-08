d1f5352987970a43aa43fc8e2f27bd0bb0ab8408
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-06-08 14:55:59 -0700

    add new account working

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -3,6 +3,7 @@ const knexDb = require("../data/dbConfig.js");
 
 const router = express.Router()
 
+//GETs
 router.get('/', (req, res) => {
      //get a list of all the accounts ins the 'accounts' table
      //select * from accounts
@@ -21,6 +22,60 @@ router.get('/', (req, res) => {
                })
           })
 })
+
+router.get('/:id', (req, res) => {
+     const accountId = req.params.id 
+     knexDb
+          .select('*')
+          .from('accounts')
+          .where({
+               id: accountId
+          })
+          .then(account => {
+               res.status(200).json({
+                    data: account
+               })
+          })
+          .catch(err => {
+               console.log('GET / error', err)
+               res.status(500).json({
+                    error: err,
+                    message: 'Error retrieving account'
+               })
+          })
+})
+
+//POSTs
+router.post(
+     '/',
+     requiredProperty('name'),
+     requiredProperty('budget'),
+     (req, res) => {
+          //always validate data coming from the client before calling the database
+          //NEVER TRUST THE CLIENT
+          const newAccount = req.body
+          knexDb('accounts')
+          .insert(newAccount, 'id')
+          .then(([id]) => {
+               res.status(201).json({newAccountId: id})
+          })
+          .catch(err => {
+               console.log('POST / error', err)
+               res.status(500).json({ message: err.message })
+          })
+     }
+)
+
+//middleware
+   function requiredProperty(property) {
+     return function(req, res, next){
+       if(!req.body[property]) {
+         res.status(400).json({ message: `Needs to have a ${property} property` })
+       } else {
+         next()
+       }
+     }
+   }
 
 
 
